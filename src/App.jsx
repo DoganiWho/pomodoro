1dca039bb34714c231f832d00f2719318ada5784
Leslie Skog <96683644+DoganiWho@users.noreply.github.com>
2023-02-12 17:01:02 +0000

    added two divs with buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <main>
       React ⚛️ + Vite ⚡ + Replit 🌀
+      <div id="break-label" >Break Length
+       <button id="break-decrement"></button>
+       <button id="break-increment"></button>
+      </div>
+      <div id="session-label" >Session Length
+       <button id="session-decrement"></button>
+       <button id="session-increment"></button>
+      </div>
+
     </main>
   )
 }
